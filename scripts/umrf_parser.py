bd99c0df2b27db1520c8507e5b37a6f0f2d901a3
SouLeo <slwanna@utexas.edu>
2022-01-09 18:07:57 -0700

    tested loading model

--- scripts/umrf_parser.py
+++ scripts/umrf_parser.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
-import os
+
 import rospy
 from std_msgs.msg import String
 import torch
 import transformers
+import t5_prompt_creation
 
 device = None
-path = os.getcwd()
-model_path = path + '/umrf_t5_small_fine_tuned'
-tokenizer_path = path + '/saved_tokenizer'
 
-model = transformers.T5ForConditionalGeneration.from_pretrained(model_path)
+model = transformers.T5ForConditionalGeneration.from_pretrained('./saved_model')
 model.eval()
-tokenizer = transformers.T5Tokenizer.from_pretrained(tokenizer_path)
+tokenizer = transformers.T5Tokenizer.from_pretrained('./saved_tokenizer')
 
 pub = rospy.Publisher('umrf_parses_output', String, queue_size=10)
+exs = []
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + "I heard: %s", data.data)
     
-    encoded_input = tokenizer(data.data, max_length=512, padding='longest', truncation=True, return_tensors='pt').data
+    ex = [data.data]
+    inputs = t5_prompt_creation.ensemble_prompt_creation(10, ex)
     
+    encoded_input = tokenizer(inputs, max_length=512, padding='longest', truncation=True, return_tensors='pt').data
     input_ids = encoded_input['input_ids'].to(device)
-    print(input_ids)
+ 
 
-    umrf_parse = model.generate(input_ids, max_length=1000)
+    umrf_parse = model.generate(input_ids, max_length=1000).squeeze()
     
-    print(umrf_parse[0])
-    pred = tokenizer.decode(umrf_parse[0])
+    pred = tokenizer.decode(umrf_parse, skip_special_tokens=True)
     
     pub.publish(pred)
 
@@ -37,6 +37,7 @@ def listener():
     rospy.init_node('umrf_parser_node', anonymous=True)
 
     rospy.Subscriber("umrf_parses", String, callback)
+
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
@@ -49,6 +50,7 @@ if __name__ == '__main__':
     else:
         device = torch.device('cpu')
         print('using cpu')
+    
     try:
         listener()
     except rospy.ROSInterruptException:
